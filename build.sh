aac8154603cc823c1c5fc5389d3980ea2be55221
takesei <sei06k14@gmail.com>
2019-10-03 16:59:23 +0900

    :wrench: Add build script to download dataset

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1Kcnru7L-iMs3uDRZt3Y0bbkYgMLloM7F' -O trashes.zip
-
+mkdir trashes
+unzip -qq -n trashes.zip
+cd trashes && rm -rf __MACOSX
+cd ../
+rm trashes.zip
